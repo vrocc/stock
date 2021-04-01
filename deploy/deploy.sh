ed13eab6e5fa4d095340c1911ba7264ae6bef385
chenpeng57 <chenpeng57@meituan.com>
2021-04-01 20:48:56 +0800

    add shell

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,5 +7,5 @@ sleep 3
 yes | cp -rf cloud_quick_demo/target/cloud-quick_demo.jar /home/cloud-quick_demo.jar
 
 cd ..
-nohup java -jar cloud-quick_demo.jar --server.port=80 >> info.log 2>&1
+nohup java -jar cloud-quick_demo.jar --server.port=80 >> info.log 2>&1 &
 exit 0
