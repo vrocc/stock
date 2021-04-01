2d7927114b66ccd8610d67b7cfba7ffbd5f29c1d
chenpeng57 <chenpeng57@meituan.com>
2021-04-01 20:33:28 +0800

    add shell

---
+++ deploy/deploy.sh
@@ -0,0 +1,6 @@
+cd /home/stock
+mvn clean package -Dmaven.test.skip=true
+ps aux|grep java|grep jar|awk '{print $2}'|xargs kill
+yes | cp -rf cloud_quick_demo/target/cloud-quick_demo.jar /home/cloud-quick_demo.jar
+cd ..
+nohup java -jar cloud-quick_demo.jar --server.port=80 >> info.log 2>&1 & tail -f info.log
