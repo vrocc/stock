409d9b321e21f8528f12fce157ceacb6ceb8df1d
chenpeng57 <chenpeng57@meituan.com>
2021-04-01 20:46:42 +0800

    add shell

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,9 @@
-cd /home/stock
-git -c credential.helper= -c core.quotepath=false -c log.showSignature=false fetch origin --recurse-submodules=no --progress --prune
-git pull
-mvn clean package -Dmaven.test.skip=true
+cd /home/stock && \
+git -c credential.helper= -c core.quotepath=false -c log.showSignature=false fetch origin --recurse-submodules=no --progress --prune && \
+git pull && \
+mvn clean package -Dmaven.test.skip=true && \
 ps aux|grep java|grep jar|awk '{print $2}'|xargs kill
+sleep 3
 yes | cp -rf cloud_quick_demo/target/cloud-quick_demo.jar /home/cloud-quick_demo.jar
 
 cd ..
