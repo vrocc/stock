87ac75c4cefc41379d9e2d1c80c30860d6e914e0
chenpeng57 <chenpeng57@meituan.com>
2021-04-01 20:37:56 +0800

    add shell

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,3 +4,4 @@ ps aux|grep java|grep jar|awk '{print $2}'|xargs kill
 yes | cp -rf cloud_quick_demo/target/cloud-quick_demo.jar /home/cloud-quick_demo.jar
 cd ..
 nohup java -jar cloud-quick_demo.jar --server.port=80 >> info.log 2>&1 & tail -f info.log
+exit 0
